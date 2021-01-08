68b18d12248242c85e68b2db459c4e532d022b79
avalos <christian@avalosPC.com>
2021-01-07 23:03:41 -0600

    modificacion en CorsFilter

--- src/main/java/ues/occ/edu/sv/ingenieria/prn335/serverpeople/resources/CorsFilter.java
+++ src/main/java/ues/occ/edu/sv/ingenieria/prn335/serverpeople/resources/CorsFilter.java
@@ -20,15 +20,20 @@ public class CorsFilter implements ContainerResponseFilter{
     @Override
     public void filter(ContainerRequestContext requestContext, 
       ContainerResponseContext responseContext) throws IOException {
-          responseContext.getHeaders().add(
-            "Access-Control-Allow-Origin", "*");
-          responseContext.getHeaders().add(
-            "Access-Control-Allow-Credentials", "true");
-          responseContext.getHeaders().add(
-           "Access-Control-Allow-Headers",
-           "origin, content-type, accept, authorization");
-          responseContext.getHeaders().add(
-            "Access-Control-Allow-Methods", 
-            "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+//          responseContext.getHeaders().add(
+//            "Access-Control-Allow-Origin", "*");
+//          responseContext.getHeaders().add(
+//            "Access-Control-Allow-Credentials", "true");
+//          responseContext.getHeaders().add(
+//           "Access-Control-Allow-Headers",
+//           "origin, content-type, accept, authorization");
+//          responseContext.getHeaders().add(
+//            "Access-Control-Allow-Methods", 
+//            "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
+        responseContext.getHeaders().putSingle("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, jwt, mensaje");
+        //response.getHeaders().putSingle("Access-Control-Expose-Headers", "jwt, mensaje");
     }
 }
